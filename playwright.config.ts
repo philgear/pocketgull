78ee9754e707bd606310b323c07ae0d4c3968f30
philgear <philgear@gmail.com>
2026-07-31 09:19:27 -0700

    fix(ci): enable reuseExistingServer in Playwright config for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   webServer: {
     command: 'npm run preview',
     url: 'http://127.0.0.1:4000',
-    reuseExistingServer: !process.env['CI'],
+    reuseExistingServer: true,
     timeout: 120 * 1000,
     env: {
       PORT: '4000',
